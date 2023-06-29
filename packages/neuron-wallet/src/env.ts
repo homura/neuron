6a3afbc0f09ac2941e62512a375d1897daa3ec2c
homura <homura.dev@gmail.com>
2023-06-29 20:37:05 +0800

    chore: invalid change and lock file

--- packages/neuron-wallet/src/env.ts
+++ packages/neuron-wallet/src/env.ts
@@ -36,8 +36,8 @@ const env = {
   app,
   isDevMode,
   isTestMode,
-  fileBasePath: path.resolve(app.getPath('userData'), fileBase),
-  mainURL: isDevMode ? 'http://localhost:3000' : `file://${path.join(__dirname, '../dist/neuron-ui/index.html')}`,
+  fileBasePath:     path.resolve(app.getPath('userData'), fileBase),
+  mainURL:          isDevMode ? 'http://localhost:3000' : `file://${path.join(__dirname, '../dist/neuron-ui/index.html')}`,
 }
 
 export default env
